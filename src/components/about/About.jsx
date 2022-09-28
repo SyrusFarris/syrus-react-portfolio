630a1e17de9237e7ed6bd7cd37c28e8aacb4f530
Syrus <syrusfarris@gmail.com>
2022-09-28 16:46:26 -0500

    finished basic template for about me section. I need to go back and put relative information later

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/Me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {BsFolder} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>About
+      <h5>Get To Know</h5>
+      <h2> About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>University of Texas Bootcamp Graduate</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>Team Player</small>
+            </article>
+
+            <article className='about__card'>
+              <BsFolder className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum similique distinctio deleniti enim aliquam, sunt perferendis nobis soluta ab quo doloremque accusantium esse saepe, labore commodi hic facilis velit natus!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'> Let's Chat!</a>
+        </div>
+      </div>
+      </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {FiUser} from 'react-icons/fi'
 import {BiBook} from 'react-icons/bi'
 import {MdComputer} from 'react-icons/md'
 import {AiOutlineMessage} from 'react-icons/ai'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><FiHome /></a>
-      <a href="#about"><FiUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#portfolio"><MdComputer /></a>
-      <a href="#contact"><AiOutlineMessage /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FiHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FiUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdComputer /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage /></a>
     </nav>
   )
 }
