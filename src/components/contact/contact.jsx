d9d0b8428162dfd8385adfa553e7ac1cde778799
Syrus <syrusfarris@gmail.com>
2022-09-29 01:19:30 -0500

    finished the contact and footer section. portfolio still needs projects attached and deployment prep

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jx5sezo', 'template_htzr2hq', form.current, 'MuxRQDQwteCJe3sVv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <container className="contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>syrusfarris@gmail.com</h5>
+            <a href="mailto:syrusfarris@gmail.com" target="_blank">Send an email</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' require />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </container>
+    </section>
   )
 }
 
